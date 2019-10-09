b3bfc3369442407643ff29184103c2ff9d49cf78
anderuraga <ander.uraga@gmail.com>
2019-10-09 13:31:02 +0200

    menu funcionando, TODO componente

--- rutas/js/app.js
+++ rutas/js/app.js
@@ -4,4 +4,40 @@ app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
 
   $scope.menuSuperior = "parciales/menu.html";
 
+  $scope.rutas = [
+    {
+      "nombre": "Inicio",
+      "url": "#!/",
+      "active": false
+    },
+    {
+      "nombre": "Profesores",
+      "url": "#!/profesores",
+      "active": true
+    },
+    {
+      "nombre": "Componente Boton dentro de HTML",
+      "url": "#!/componente1",
+      "active": false
+    },
+    {
+      "nombre": "Componente Boton sin html",
+      "url": "#!/componente2",
+      "active": false
+    },
+    {
+      "nombre": "Creditos",
+      "url": "#!/creditos",
+      "active": false
+    }
+  ];
+
+  $scope.cambiarActivo = function( ruta ){
+    console.trace('click cambiarActivo %o', ruta);
+    $scope.rutas.forEach(element => {
+      element.active = false;
+    });
+    ruta.active = true;
+  }
+
 }]);
