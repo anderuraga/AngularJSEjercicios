25b567cb31175b31c1bfd76647d7d5c55ada65c1
anderuraga <ander.uraga@gmail.com>
2019-10-15 13:20:40 +0200

    todo

--- rutas/js/app.js
+++ rutas/js/app.js
@@ -14,47 +14,8 @@ var app = angular.module('angularApp',['ngRoute','ngSanitize']);
                                     } );
 
 /**
- *  Providers
+ *  Servicio o Provider
  */
-
-function CancionProvider($http){
-
-  console.log('CancionProvider');
-  const ENDPOINT = "http://localhost:8080/cancion/";
-
-  this.listar = function(){    
-    console.log('cancionProvider listar ' + ENDPOINT);
-    return $http.get(ENDPOINT);
-
-  }// listar
-
-  this.detalle = function( idCancion ){    
-    let url = ENDPOINT + idCancion;
-    console.log('cancionProvider detalle ' + url);
-    
-  }// detalle
-
-  this.eliminar = function( idCancion ){    
-    let url = ENDPOINT + idCancion;
-    console.log('cancionProvider eliminar ' + url);
-    
-  }// eliminar
-
-
-  this.crear = function( nombreCancion ){    
-    let url = ENDPOINT;
-    console.log('cancionProvider nombreCancion ' + url);
-    
-  }// crear
-
-  this.modificar = function( idCancion, nombreCancion ){    
-    let url = ENDPOINT  + idCancion;
-    console.log('cancionProvider modificar %s  id=%s nombre=%s', url, idCancion, nombreCancion );
-    
-  }// modificar
-
-
-}
 
  app.service("cancionProvider", CancionProvider );
 
