44fbfac1b4444ebb458e054e94af24a6d93a71e9
anderuraga <ander.uraga@gmail.com>
2019-10-17 09:10:58 +0200

    formularios

--- rutas/js/formularioController.js
+++ rutas/js/formularioController.js
@@ -5,14 +5,22 @@ app.controller('formularioController', ['$scope', 'servicioConstantes',
     console.trace('formularioController');    
 
     $scope.titulo = "Formularios";
-    $scope.fomulario = {
+
+
+    $scope.formData = {};
+    /*
+    $scope.formData = {
         "email" : servicioConstantes.email,
         "nombre" : servicioConstantes.autor
-    };
+    };*/
+
 
     $scope.postear = ()=>{
+        console.trace('formulario sumitado %o', $scope.formData );
+    }
 
-        console.trace('formulario sumitado %o', $scope.fomulario );
+    $scope.changeStiles = (form)=>{
+        console.trace('formulario de la vista %o', form );
     }
 
 }]);
